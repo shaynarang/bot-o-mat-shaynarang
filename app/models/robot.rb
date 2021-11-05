021ec3713662fb19c1ebef7e85513069e53cf09c
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2021-11-04 21:22:26 -0500

    associate robots with tasks

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -2,6 +2,8 @@
 
 class Robot < ApplicationRecord
   belongs_to :user
+  has_many :robots_tasks, dependent: :destroy
+  has_many :tasks, through: :robots_tasks
 
   enum kind: { unipedal: 0, bipedal: 1, quadrupedal: 2, arachnid: 3, radial: 4, aeronautical: 5 }
 end
