9f5b3a32d9b32683384b8cd404a44956642a6eaf
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2021-11-06 00:30:50 -0500

    install bootstrap and popper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,28 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("styles/styles")
+require("@popperjs/core")
+require("../stylesheets/application.scss")
+
+// Bootstrap
+import "bootstrap"
+
+// Import the specific modules you may need (Modal, Alert, etc)
+import { Tooltip, Popover } from "bootstrap"
+
+// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
+document.addEventListener("turbolinks:load", () => {
+  // Both of these are from the Bootstrap 5 docs
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+      return new Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+      return new Popover(popoverTriggerEl)
+  })
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,5 +37,3 @@ require("styles/styles")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-global.$ = require('jquery')
