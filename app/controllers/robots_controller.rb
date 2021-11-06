1906f684a7acd01dc9caa1c2399bace66b8adfe4
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2021-11-05 23:44:05 -0500

    add robots controller

---
+++ app/controllers/robots_controller.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+class RobotsController < ApplicationController
+  before_action :set_robot, only: %i[ show edit update destroy ]
+
+  # GET /robots
+  def index
+    @robots = current_user.robots
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render :index }
+    end
+  end
+
+  # GET /robots/1
+  def show
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render :show }
+    end
+  end
+
+  # GET /robots/new
+  def new
+    @robot = Robot.new
+  end
+
+  # GET /robots/1/edit
+  def edit
+  end
+
+  # POST /robots or /robots.json
+  def create
+    @robot = Robot.new(robot_params)
+
+    respond_to do |format|
+      if @robot.save
+        format.html { redirect_to robots_url, notice: 'Robot was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /robots/1
+  def update
+    respond_to do |format|
+      @robot.attributes = {'task_ids' => []}.merge(robot_params || {})
+      if @robot.update(robot_params)
+        format.html { redirect_to robots_url, notice: 'Robot was successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.html { redirect_to :edit, error: @robot.errors.full_messages }
+      end
+    end
+  end
+
+  # DELETE /robots/1
+  def destroy
+    @robot.destroy
+    respond_to do |format|
+      format.html { redirect_to robots_url, notice: 'Robot was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_robot
+    @robot = Robot.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def robot_params
+    params.require(:robot).permit(:name, :kind, :user_id, :task_ids => [])
+  end
+end
