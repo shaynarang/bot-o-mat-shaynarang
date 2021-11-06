fe35b67f4bb93bdeb1f9f7980ff6e7f30f5141c0
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2021-11-06 17:37:22 -0500

    disable instead of hide inputs

--- app/javascript/packs/robot.js
+++ app/javascript/packs/robot.js
@@ -2,27 +2,35 @@ $(document).on('turbolinks:load', function() {
   // locate the robot kind select
   var kind = $('select#robot_kind');
 
-  // ensure unchecking of boxes that are hidden
-  function uncheckHiddenBoxes() {
+  // disable tasks with mobility requirements
+  function disableTasksWithMobilityRequirements() {
     var checkboxes = $('.requires_mobility').find('input[type=checkbox]')
     checkboxes.each(function(){ 
+      $(this).prop('disabled', true)
+      // ensure unchecking of boxes that are disabled
       $(this).prop('checked', false)
     });
   }
 
-  // toggle tasks requiring mobility upon kind select change
+  // enable tasks with mobility requirements
+  function enableTasksWithMobilityRequirements() {
+    var checkboxes = $('.requires_mobility').find('input[type=checkbox]')
+    checkboxes.each(function(){
+      $(this).prop('disabled', false)
+    });
+  }
+
+  // toggle ability of tasks with mobility requirements upon kind select change
   kind.on('change', function() {
     if (this.value == 'unipedal'){
-      uncheckHiddenBoxes()
-      $('.requires_mobility').slideUp()
+      disableTasksWithMobilityRequirements()
     } else {
-      $('.requires_mobility').slideDown()
+      enableTasksWithMobilityRequirements()
     }
   });
 
-  // ensure hidden boxes upon page load if kind is unipedal
+  // ensure tasks with mobility requirements are disabled upon page load if kind is unipedal
   if ($(kind).val() == 'unipedal') {
-    uncheckHiddenBoxes()
-    $('.requires_mobility').hide()
+    disableTasksWithMobilityRequirements()
   }
 });
