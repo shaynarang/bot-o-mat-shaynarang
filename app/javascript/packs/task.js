eb81354d0310a3b777bc7170aec5ec42c18eeeb0
Shay Narang <shaynarang@admins-MBP-3.MG8702>
2021-12-14 14:08:00 -0600

    refactor to use es6 function syntax, let, const, and template string literals

--- app/javascript/packs/task.js
+++ app/javascript/packs/task.js
@@ -1,7 +1,7 @@
-$(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load', () => {
   function appendEntriesToLog(log, entries, delay) {
     // Consider delay (duration) prior to append.
-    window.setTimeout(function() {
+    window.setTimeout(() => {
       $('<ul/>', { html: entries.join( '' ) })
         .appendTo(log);
       log.fadeIn();
@@ -13,13 +13,13 @@ $(document).on('turbolinks:load', function() {
     // Convert duration to seconds.
     duration = duration / 1000
     // Account for singular second.
-    var unit = duration > 0 ? ' seconds' : ' second'
+    const unit = duration > 0 ? ' seconds' : ' second';
 
     items = [];
-    $.map(tasks, function(task) {
-      items.push('<li>' + name + ' completed ' + task + '</li>')
+    $.map(tasks, task => {
+      items.push(`<li>${name} completed ${task}</li>`)
     });
-    items.push('<li>' + name + ' took ' + duration + unit + '</li>')
+    items.push(`<li>${name} took ${duration}${unit}</li>`)
     return items
   }
 
@@ -30,8 +30,8 @@ $(document).on('turbolinks:load', function() {
     // Convert duration to seconds.
     total_duration = total_duration / 1000
 
-    var count = 0;
-    var counter = setInterval(function() {
+    let count = 0;
+    const counter = setInterval(() => {
       ++count;
       $(timer).html(count);
       // When the timer has met the total duration.
@@ -39,7 +39,7 @@ $(document).on('turbolinks:load', function() {
         // Stop.
         clearInterval(counter);
         // Display the total duration.
-        var unit = total_duration > 1 ? ' seconds' : ' second'
+        const unit = total_duration > 1 ? ' seconds' : ' second';
         $(timer).html(total_duration + unit);
         // Reenable run tasks link.
         link.removeClass('disabled');
@@ -63,26 +63,26 @@ $(document).on('turbolinks:load', function() {
       width: '100%'
     }, {
       queue: false,
-      duration: duration
+      duration
     })
   }
 
   // Obtain data and run tasks in batches.
   function runTasks(robot_id, progress_bar, timer, log, link) {
     // Hit show endpoint to acquire robot data.
-    $.getJSON( '/robots/' + robot_id  + '.json', function(data) {
-      var name = data['name']
-      var batches = data['tasks_batch_info']
-      var total_duration = data['tasks_duration']
-      var accumulated_duration = 0;
+    $.getJSON( `/robots/${robot_id}.json`, data => {
+      const name = data['name'];
+      const batches = data['tasks_batch_info'];
+      const total_duration = data['tasks_duration'];
+      let accumulated_duration = 0;
 
       // Iterate over batches of tasks.
-      $.map(batches, function(batch){
-        var batch_duration = batch['duration']
-        var tasks = batch['tasks'];
+      $.map(batches, batch => {
+        const batch_duration = batch['duration'];
+        const tasks = batch['tasks'];
 
         // Formulate log entries for current batch.
-        var log_entries = formulateLogEntries(name, batch_duration, tasks);
+        const log_entries = formulateLogEntries(name, batch_duration, tasks);
 
         accumulated_duration += batch_duration;
 
@@ -97,12 +97,12 @@ $(document).on('turbolinks:load', function() {
   $('a.run_tasks').on('click', function(event) {
     event.preventDefault();
 
-    var robot_id = $(this).data('robot-id');
+    const robot_id = $(this).data('robot-id');
 
-    var progress_bar = $("div.progress_bar[data-robot-id='" + robot_id + "']")
-    var timer = $("div.timer[data-robot-id='" + robot_id + "']")
-    var log = $("div.log[data-robot-id='" + robot_id + "']")
-    var link = $(this);
+    const progress_bar = $(`div.progress_bar[data-robot-id='${robot_id}']`);
+    const timer = $(`div.timer[data-robot-id='${robot_id}']`);
+    const log = $(`div.log[data-robot-id='${robot_id}']`);
+    const link = $(this);
 
     // Disable run tasks link.
     link.addClass('disabled');
