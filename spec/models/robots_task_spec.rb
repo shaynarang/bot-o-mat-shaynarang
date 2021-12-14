1234607a2d1792a7aa435b9c188cdadc4a320468
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2021-12-13 20:15:57 -0600

    refactor per rubocop

--- spec/models/robots_task_spec.rb
+++ spec/models/robots_task_spec.rb
@@ -3,46 +3,48 @@
 require 'rails_helper'
 
 RSpec.describe RobotsTask, type: :model do
-  let(:user) {
+  let(:user) do
     User.create(email: 'user@example.com',
                 password: 'password',
                 password_confirmation: 'password')
-  }
+  end
 
-  let(:robot) {
+  let(:robot) do
     Robot.create(name: 'Foo',
                  kind: 'bipedal',
                  user: user)
-  }
+  end
 
-  let(:task) {
+  let(:task) do
     Task.create(description: 'Desc',
-                eta: 20000)
-  }
+                eta: 20_000)
+  end
 
-  subject {
+  let(:robots_task) do
     described_class.new(robot: robot,
                         task: task)
-  }
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
   end
 
-  it 'is not valid without a robot' do
-    subject.robot = nil
-    expect(subject).to_not be_valid
-  end
+  describe 'validation' do
+    it 'fails without a robot' do
+      robots_task.robot = nil
+      expect(robots_task).to_not be_valid
+      error = /Robot must exist/
+      expect { robots_task.save! }.to raise_error(error)
+    end
 
-  it 'is not valid without a task' do
-    subject.task = nil
-    expect(subject).to_not be_valid
-  end
+    it 'fails without a task' do
+      robots_task.task = nil
+      expect(robots_task).to_not be_valid
+      error = /Task must exist/
+      expect { robots_task.save! }.to raise_error(error)
+    end
 
-  it 'prevents duplicate entries' do
-    robot.tasks << task # task already associated
-    expect(subject).to_not be_valid
-    error = /Robot has already been taken/
-    expect{ subject.save! }.to raise_error(error)
+    it 'prevents duplicate entries' do
+      robot.tasks << task # task already associated
+      expect(robots_task).to_not be_valid
+      error = /Robot has already been taken/
+      expect { robots_task.save! }.to raise_error(error)
+    end
   end
 end
