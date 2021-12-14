c1246528d3fb9aea5935c148169db9b6633527e4
Shay Narang <shaynarang@admins-MBP-3.MG8702>
2021-12-14 14:31:37 -0600

    refactor to use es6 function syntax and const declarations

--- app/javascript/packs/robot.js
+++ app/javascript/packs/robot.js
@@ -1,26 +1,26 @@
-$(document).on('turbolinks:load', function() {
-  // locate the robot kind select
-  var kind = $('select#robot_kind');
+$(document).on('turbolinks:load', () => {
+  // Locate the robot kind select.
+  const kind = $('select#robot_kind');
 
-  // disable tasks with mobility requirements
+  // Disable tasks with mobility requirements.
   function disableTasksWithMobilityRequirements() {
-    var checkboxes = $('.requires_mobility').find('input[type=checkbox]')
+    const checkboxes = $('.requires_mobility').find('input[type=checkbox]');
     checkboxes.each(function(){ 
       $(this).prop('disabled', true)
-      // ensure unchecking of boxes that are disabled
+      // Ensure unchecking of boxes that are disabled.
       $(this).prop('checked', false)
     });
   }
 
-  // enable tasks with mobility requirements
+  // Enable tasks with mobility requirements.
   function enableTasksWithMobilityRequirements() {
-    var checkboxes = $('.requires_mobility').find('input[type=checkbox]')
+    const checkboxes = $('.requires_mobility').find('input[type=checkbox]');
     checkboxes.each(function(){
       $(this).prop('disabled', false)
     });
   }
 
-  // toggle ability of tasks with mobility requirements upon kind select change
+  // Toggle ability of tasks with mobility requirements upon kind select change.
   kind.on('change', function() {
     if (this.value == 'unipedal'){
       disableTasksWithMobilityRequirements()
@@ -29,7 +29,7 @@ $(document).on('turbolinks:load', function() {
     }
   });
 
-  // ensure tasks with mobility requirements are disabled upon page load if kind is unipedal
+  // Ensure tasks with mobility requirements are disabled upon page load if kind is unipedal.
   if ($(kind).val() == 'unipedal') {
     disableTasksWithMobilityRequirements()
   }
